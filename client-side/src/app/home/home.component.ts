2fed297dc6064e4b3755058f5ecc6bfb8030204a
michael.a@pepperi.com <michael.a@pepperi.com>
2024-10-21 02:16:29 +0300

    init home component

--- client-side/src/app/home/home.component.ts
+++ client-side/src/app/home/home.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  winner: string | null = null; // Store the winner
 
+  onGameOver(winner: string): void {
+    this.winner = winner; // Set the winner when the game ends
+  }
+
+  startNewGame(): void {
+    this.winner = null; // Reset the winner
+    console.log('New game started!');
+    // Additional logic to trigger game setup (to be added later)
+  }
 }
